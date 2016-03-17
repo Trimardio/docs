b9e07edb928f6aeda9d22c473227756e4b43b495
Trimardio <geoffroy.schmitlin@phiropsi.com>
2016-03-17 20:48:13 +0100

    Added support for .txt files. Added font-awesome. Added mermaid.js and analytics. Refacto menu.

--- conf.py
+++ conf.py
@@ -43,7 +43,7 @@ source_parsers = {
 }
 
 # The suffix of source filenames.
-source_suffix = '.md'
+source_suffix = '.md', '.txt'
 
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'
@@ -137,7 +137,7 @@ html_title = "Hyperboria Documentation"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ['_static']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
